bad6d1f0478d91b7ec740afe1d994dae4e9b5223
Paul Merrill <pdm@pdm.me>
2017-04-30 07:48:21 -0700

    pmer-macvim: New formula

---
+++ Formula/pmer-macvim.rb
@@ -0,0 +1,22 @@
+class PmerMacvim < Formula
+  head "https://github.com/pmer/macvim.git"
+
+  def install
+    system "./configure", "--enable-multibyte",
+                          "--with-macarchs=#{MacOS.preferred_arch}",
+                          "--with-tlib=ncurses",
+                          "--with-compiledby=Homebrew",
+                          "--with-local-dir=#{HOMEBREW_PREFIX}"
+
+    system "make"
+
+    prefix.install "src/MacVim/build/Release/MacVim.app"
+    inreplace "src/MacVim/mvim", %r{^# VIM_APP_DIR=\/Applications$},
+                                 "VIM_APP_DIR=#{prefix}"
+    bin.install "src/MacVim/mvim"
+
+    # Create MacVim vimdiff, view, ex equivalents
+    executables = %w[mvimdiff mview mvimex gvim gvimdiff gview gvimex]
+    executables.each { |e| bin.install_symlink "mvim" => e }
+  end
+end
