97962f5063813986b80517991e0989cb1cec394e
Paul Merrill <pdm@pdm.me>
2017-04-30 07:31:59 -0700

    pmer-vim: Simplify formula

--- Formula/pmer-vim.rb
+++ Formula/pmer-vim.rb
@@ -2,25 +2,18 @@ class PmerVim < Formula
   head "https://github.com/pmer/vim.git"
 
   def install
-    opts = []
-
-    opts << "--disable-nls"
-    opts << "--enable-gui=no"
-    opts << "--without-x"
-
-    # We specify HOMEBREW_PREFIX as the prefix to make vim look in the
-    # the right place (HOMEBREW_PREFIX/share/vim/{vimrc,vimfiles}) for
-    # system vimscript files. We specify the normal installation prefix
-    # when calling "make install".
     system "./configure", "--prefix=#{HOMEBREW_PREFIX}",
                           "--mandir=#{man}",
                           "--enable-multibyte",
                           "--with-tlib=ncurses",
-                          *opts
+                          "--disable-nls",
+                          "--enable-gui=no",
+                          "--without-x"
+
     system "make"
-    # Parallel install could miss some symlinks
-    # https://github.com/vim/vim/issues/1031
+
     ENV.deparallelize
+
     system "make", "install", "prefix=#{prefix}"
   end
 end
